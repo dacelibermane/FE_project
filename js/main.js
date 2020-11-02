2d2e90722aacfe7d2cdd3c5309ed0f8eb7cc92d8
dacelibermane <dace.libermane@gmail.com>
2020-11-02 15:30:20 +0200

    local storage added

--- js/main.js
+++ js/main.js
@@ -1,109 +1,114 @@
 window.addEventListener('load', function(){
 
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const saveBtn = document.getElementById('submitBtn');
+
+
+
+
 //The form popup after clicking the button
-  document.getElementById('btn').addEventListener('click', function(){
-   document.querySelector('.popup-form').style.display = 'flex';
+document.getElementById('btn').addEventListener('click', function(){
+  document.querySelector('.popup-form').style.display = 'flex';
 });
 
 //The form closes after clicking "X"
-  document.querySelector('.close').addEventListener('click', function(){
-    document.querySelector('.popup-form').style.display = 'none';
+document.querySelector('.close').addEventListener('click', function(){
+  document.querySelector('.popup-form').style.display = 'none';
 });
 
-//User validation
-  document.getElementById('btn').addEventListener('click', function() {
-    const form = document.getElementById('form').elements;
-
+//show hide elements by clicking on checkbox
+document.getElementById('btn').addEventListener('click', function showHide(){
+  if(document.getElementById('checkbox').checked){  
+      document.getElementById('receiveOffers').style.display='block';
+  }else{
+    document.getElementById('receiveOffers').style.display='none';
+  }
+});
   
 
-    if(isFormValid(form)) {
-      console.log('can be saved')
-    }else{
-      console.log('not valid')
-    }
+//form validation
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
 
+  checkInputs();
+});
+
+
+function checkInputs(){
+  //getting values form input
+  const usernameValue = username.value.trim();
+  const emailValue = email.value.trim();
+  const passwordValue = password.value.trim();
+
+  if(usernameValue === '') {
+      showError(username, 'Username cannot be blank');
+  }else{
+      showSuccess(username);
+  }
+
+  if(emailValue === ''){
+    showError(email, 'Email cannot be blank');
+  }else if(!isEmail(emailValue)) {
+    showError(email, 'Email is not valid');
+  }else{
+    showSuccess(email);
+  }
+
+  
+  if(passwordValue === ''){
+    showError(password, 'Password cannot be blank');
+  }else{
+    showSuccess(password);
+  }
+
+  // alert(usernameValue + " , thank you! We received your request")
+
+
+}
+
+function showError(input, message) {
+  const formControl = input.parentElement;
+  const errorMsg = formControl.querySelector('small');
+
+  errorMsg.innerText = message;
+
+  formControl.className = 'input-wrapper error';
+}
+
+function showSuccess(input, message){
+  const formControl = input.parentElement;
+  formControl.className = 'input-wrapper success';
+}
+
+function isEmail(email){
+  return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
+
+// saving to local storage
+saveBtn.onclick = function (){
+  const saveUsername = username.value;
+  const saveEmail = email.value;
+
+  console.log(saveUsername);
+  console.log(saveEmail);
+
+  if(saveUsername && saveEmail){
+    localStorage.setItem(saveUsername, saveEmail);
+    location.reload();
+  }
+};
 
 });
 
-  function isFormValid(form) {
-    let isFormValid = true;
 
-    const username = form.namedItem('username').value;
-    console.log(username);
-    return isFormValid;
-  }
-
-
-
-
-})
-
-
-
-
-// / document.getElementById('submitBtn').addEventListener('click', function(){
-//   const form = document.getElementById('form').elements;
-
-//     if (isFormValid(form)) {
-//      let userList = localStorage.userList;
- 
-//      if(userList) {
-//        userList = JSON.parse(userList);
-//       }else{
-//         userList = [];
-//       }
-
-//       const user = {
-//         username: form.namedItem('username').value,
-//         email: form.namedItem('email').value,
-//         password: form.namedItem('password').value
-//       };
-//         userList.push(JSON.stringify(user));
-//         localStorage.userList = JSON.stringify(userList);
-    
-//     console.log('can be saved')
-//     }else{
-//   console.log('form not valid')
-//  }
-// });
- 
-// function isFormValid(form) {
-// let isFormValid = true;
-
-// const name = form.namedItem('name').value;
-// if(name.lenght < 3) {
-//  const errorMsg = document.querySelector('.error-msg name')[0];
-//  errorMsg.innerHTML = "Please enter valid name is required"
-//  isFormValid = false;
-// }
-
-// const email = form.namedItem('email').value;
-// const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
-// if(!re.test(email)){
-//  isFormValid = false;
-
-// }
-
-
-
-// return isFormValid;
-// }
 
 
 
  
-// //Select appears, When checkbox is checked
-// let checkbox = document.getElementById('sendOffers');
-// let receiveOffers = document.getElementById('receiveOffers');
 
-// checkbox.addEventListener('click', function (){
-//   if(checkbox.checked){
-//     receiveOffers.style.display = 'block';
-//   }else{
-//     receiveOffers.style.display = 'none';
-//   }
-// })
   
    
 

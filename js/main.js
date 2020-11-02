6718c0a86647994084a4d294304882626daffce2
dacelibermane <dace.libermane@gmail.com>
2020-11-02 09:03:17 +0200

    changes

--- js/main.js
+++ js/main.js
@@ -1,37 +1,124 @@
-window.addEventListener('load', function() {
+window.addEventListener('load', function(){
 
+//The form popup after clicking the button
   document.getElementById('btn').addEventListener('click', function(){
-    document.querySelector('.popup-form').style.display = 'flex';
-  });
+   document.querySelector('.popup-form').style.display = 'flex';
+});
 
+//The form closes after clicking "X"
+  document.querySelector('.close').addEventListener('click', function(){
+    document.querySelector('.popup-form').style.display = 'none';
+});
 
+//User validation
+  document.getElementById('btn').addEventListener('click', function() {
+    const form = document.getElementById('form').elements;
 
-  document.getElementById('submitBtn').addEventListener('click', function(){
-    const form = document.getElementById('inputGroup').elements;
+  
 
-    if (isFormValid(form)) {
+    if(isFormValid(form)) {
       console.log('can be saved')
-    } else{
-      console.log('form not valid')
+    }else{
+      console.log('not valid')
     }
-  });
+
+
+});
 
   function isFormValid(form) {
     let isFormValid = true;
 
-    const userName = form.namedItem('username').value;
-
-    console.log(userName);
+    const username = form.namedItem('username').value;
+    console.log(username);
     return isFormValid;
   }
 
+
+
+
 })
 
-function showHideDiv(){
-  if(document.getElementById('sendOffers').checked) {
-    document.getElementById('receiveOffers').style.display='block';
-  }else{
-    document.getElementById('receiveOffers').style.display='none';
-  }
-};
+
+
+
+// / document.getElementById('submitBtn').addEventListener('click', function(){
+//   const form = document.getElementById('form').elements;
+
+//     if (isFormValid(form)) {
+//      let userList = localStorage.userList;
+ 
+//      if(userList) {
+//        userList = JSON.parse(userList);
+//       }else{
+//         userList = [];
+//       }
+
+//       const user = {
+//         username: form.namedItem('username').value,
+//         email: form.namedItem('email').value,
+//         password: form.namedItem('password').value
+//       };
+//         userList.push(JSON.stringify(user));
+//         localStorage.userList = JSON.stringify(userList);
+    
+//     console.log('can be saved')
+//     }else{
+//   console.log('form not valid')
+//  }
+// });
+ 
+// function isFormValid(form) {
+// let isFormValid = true;
+
+// const name = form.namedItem('name').value;
+// if(name.lenght < 3) {
+//  const errorMsg = document.querySelector('.error-msg name')[0];
+//  errorMsg.innerHTML = "Please enter valid name is required"
+//  isFormValid = false;
+// }
+
+// const email = form.namedItem('email').value;
+// const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+// if(!re.test(email)){
+//  isFormValid = false;
+
+// }
+
+
+
+// return isFormValid;
+// }
+
+
+
+ 
+// //Select appears, When checkbox is checked
+// let checkbox = document.getElementById('sendOffers');
+// let receiveOffers = document.getElementById('receiveOffers');
+
+// checkbox.addEventListener('click', function (){
+//   if(checkbox.checked){
+//     receiveOffers.style.display = 'block';
+//   }else{
+//     receiveOffers.style.display = 'none';
+//   }
+// })
+  
+   
+
+ 
+
+
+  
+
+
+
+
+   
+
+
+
+
+
 
